01c3ac65ec806a7a957adc120114490aa3ec2ae5
VazhnovPavel <pavel.fortex@yandex.ru>
2024-01-26 14:59:34 +0300

    Start

---
+++ src/main/java/com/example/psytestbalance/PsyTestBalanceApplication.java
@@ -0,0 +1,13 @@
+package com.example.psytestbalance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsyTestBalanceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PsyTestBalanceApplication.class, args);
+    }
+
+}
